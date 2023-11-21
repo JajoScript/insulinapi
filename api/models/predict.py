64cb3b60ee2fe101396ab8b0cc89d3c36e97ca4e
Gonzalo <gonzalo.cvaldebenito@gmail.com>
2023-11-20 23:49:06 -0300

    logica

--- api/models/predict.py
+++ api/models/predict.py
@@ -1,39 +1,24 @@
 import os
+import numpy as np
+from keras.preprocessing.image import load_img, img_to_array
+from keras.models import load_model
 
 # Rutas de los archivos.
 modeloPath = f"{os.getcwd()}\\api\\models\\modelo.h5"
 pesosPath = f"{os.getcwd()}\\api\\models\\pesos.h5"
+altura, longitud = 200, 200
+clases = ['BANANA', 'FRUTILLA', 'KIWI', 'NARANJA', 'PINA', 'PITAHAYA', 'SANDIA', 'UVA']
 
 async def predecir_fruta(file_path):
-    return "[🐶] Es una fruta del dragon!"
+    cnn = load_model(modeloPath)
+    cnn.load_weights(pesosPath)
 
-# import os
-# import numpy as np
-# from keras.preprocessing.image import load_img, img_to_array
-# from keras.models import load_model
+    x = load_img(file_path, target_size=(200, 200))
+    x = img_to_array(x)
+    x = np.expand_dims(x, axis=0)
+    arreglo = cnn.predict(x)
+    resultado = arreglo[0]
+    respuesta = np.argmax(resultado)
+    fruta = clases[respuesta]
 
-# altura, longitud = 200, 200
-# modelo = 'modelo.h5'  # Cambia esto a la ruta correcta de tu modelo
-# pesos = 'pesos.h5'  # Cambia esto a la ruta correcta de tus pesos
-
-# cnn = load_model(modelo)
-# cnn.load_weights(pesos)
-
-# def predict_image(file):
-#     x = load_img(file, target_size=(200, 200))
-#     x = img_to_array(x)
-#     x = np.expand_dims(x, axis=0)
-#     arreglo = cnn.predict(x)
-#     resultado = arreglo[0]
-#     respuesta = np.argmax(resultado)
-
-#     clases = ['BANANA', 'FRUTILLA', 'KIWI', 'NARANJA', 'PINA', 'PITAHAYA', 'SANDIA', 'UVA']
-#     return clases[respuesta]
-
-# # Nombre del archivo que deseas predecir
-# archivo_a_predecir = 'imagen.png'
-
-# # Realizar la predicción para el archivo especificado
-# prediccion = predict_image(archivo_a_predecir)
-
-# print(f"Predicción para {archivo_a_predecir}: {prediccion}")
+    return f"[🐶] Es una fruta: {fruta}!"
